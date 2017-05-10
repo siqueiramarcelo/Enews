028530d0e65bb13ec7742dcf27787cdc9fca9928
siqueiramarcelo <v.marcelosiqueira@gmail.com>
2017-05-10 13:11:47 -0300

    First push to production

--- app/src/main/java/com/ristana/e_news/ui/activity/AboutActivity.java
+++ app/src/main/java/com/ristana/e_news/ui/activity/AboutActivity.java
@@ -11,7 +11,7 @@ import com.ristana.e_news.R;
 
 public class AboutActivity extends AppCompatActivity {
 
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
